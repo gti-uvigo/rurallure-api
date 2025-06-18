e2c61129b7a80791b4b8f255a8321cd5a077a01a
apajonGTI <apajon@gti.uvigo.es>
2025-06-18 08:49:51 +0200

    hotfix

--- app.py
+++ app.py
@@ -159,5 +159,5 @@ def function_download_image(image_id):
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, host='127.0.0.1', port=5000)
